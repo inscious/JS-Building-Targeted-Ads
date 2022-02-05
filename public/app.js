7f092a698a7973269d6496fad677742c77b0bd48
Saul Cortes <92839820+inscious@users.noreply.github.com>
2022-02-05 12:54:16 -0800

    initial commit

--- public/app.js
+++ public/app.js
@@ -1,4 +1,10 @@
-// get user's data
+// Get the user's time:                                                              
+function getMealTime(){
+    const tod = userTime()
+    return tod > 20 ? 'latenight snack' : tod > 16 ? 'dinner' : tod > 11 ? 'lunch' : 'breakfast'
+}
+console.log(getMealTime())
+
 // get user's coordinates
 
 
@@ -10,9 +16,15 @@
 
 
 // build ads
-// build ad 1
-
-
+// Build Ad 1:                                                           
+function buildAd1(){
+    const mealTime = getMealTime()
+    let content = document.querySelector('.ad1')
+    let inner = document.createElement('p')
+    inner.innerHTML = `We've got the best <span>${mealTime}</span> in town`
+    content.append(inner)
+}
+buildAd1()
 // build ad 2
 
 
